02027f0be95c55b4da3ee1ddb22c646282d361ca
ThermoDev <michael.pegios@hotmail.com>
2020-05-18 23:17:05 +1000

    Making API Requests

---
+++ src/api/unsplash.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const UNSPLASH_API_KEY = `${process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY.toString()}`; /* Replace with your own API key from unsplash. */
+
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID ' + UNSPLASH_API_KEY.toString(),
+    },
+})
